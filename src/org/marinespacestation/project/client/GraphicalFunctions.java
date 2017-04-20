c91b2a10b49998dd8480c601a119380277270b10
JayTheDonkey <jayander3.14159@gmail.com>
2017-04-20 10:02:54 -0700

    Almost there
    still needs functionality and a bit of formatting

--- src/org/marinespacestation/project/client/GraphicalFunctions.java
+++ src/org/marinespacestation/project/client/GraphicalFunctions.java
@@ -13,41 +13,67 @@ public class GraphicalFunctions {
 
         final DialogBox dialog = new DialogBox(true);
         dialog.setPopupPosition(400, 300);
-        dialog.setText("Test BOI");
+        dialog.setText("Line of Best Fit");
         dialog.setAnimationEnabled(true);
         dialog.setGlassEnabled(true);
 
-        Button ok = new Button("OK");
+        Button ok = new Button("Enter");
         ok.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                dialog.hide();
+                dialog.hide();//for now
             }
         });
 
-        Button ko = new Button("KO!");
-        ko.addClickHandler(new ClickHandler() {
+        Button cancel = new Button("Cancel");
+        cancel.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 dialog.hide();
             }
         });
 
-        Label label = new Label("Some sample text");
+        Label label = new Label("Choose two axes");
+        Label axis1 = new Label("1st axis:");
+        Label axis2 = new Label("2nd axis:");
 
         VerticalPanel panel = new VerticalPanel();
+        panel.setStyleName("verticalPanel");
         panel.setHeight("100");
         panel.setWidth("300");
-        panel.setSpacing(10);
         panel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
         panel.add(label);
 
-        HorizontalPanel horizontalPanel = new HorizontalPanel();
-        horizontalPanel.setHeight("100");
-        horizontalPanel.setWidth("300");
-        horizontalPanel.setSpacing(50);
-        horizontalPanel.addStyleName("horizontalPanel");
-        horizontalPanel.add(ok);
-        horizontalPanel.add(ko);
-        panel.add(horizontalPanel);
+        ListBox axesBox1 = new ListBox();
+        axesBox1.addItem("X Axis");
+        axesBox1.addItem("Y Axis");
+
+        ListBox axesBox2 = new ListBox();
+        axesBox2.addItem("X Axis");
+        axesBox2.addItem("Y Axis");
+
+        HorizontalPanel listBoxLayer1 = new HorizontalPanel();
+        listBoxLayer1.setHeight("100");
+        listBoxLayer1.setWidth("300");
+        listBoxLayer1.add(axis1);
+        listBoxLayer1.add(axesBox1);
+
+        panel.add(listBoxLayer1);
+
+        HorizontalPanel listBoxLayer2 = new HorizontalPanel();
+        listBoxLayer2.setHeight("100");
+        listBoxLayer2.setWidth("300");
+        listBoxLayer2.add(axis2);
+        listBoxLayer2.add(axesBox2);
+
+        panel.add(listBoxLayer2);
+
+        HorizontalPanel buttonLayer = new HorizontalPanel();
+        buttonLayer.setHeight("100");
+        buttonLayer.setWidth("300");
+        buttonLayer.addStyleName("buttonPanel");
+        buttonLayer.add(ok);
+        buttonLayer.add(cancel);
+
+        panel.add(buttonLayer);
 
         dialog.setWidget(panel);
         dialog.center();
